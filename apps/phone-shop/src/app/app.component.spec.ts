7e239cc75e76a13fc7c60af240f02d876561abdd
Kaitlyn <kaitlyn.ekdahl@gmail.com>
2019-04-25 21:44:54 -0700

    Initial styles and quick unit test fixes

--- apps/phone-shop/src/app/app.component.spec.ts
+++ apps/phone-shop/src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, async } from "@angular/core/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 import { AppComponent } from "./app.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      declarations: [AppComponent],
+      imports: [RouterTestingModule],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     }).compileComponents();
   }));
 
@@ -18,14 +22,5 @@ describe("AppComponent", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual("phone-shop");
-  });
-
-  it("should render title in a h1 tag", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector("h1").textContent).toContain(
-      "Welcome to phone-shop!"
-    );
   });
 });
--- apps/phone-shop/src/app/app.module.ts
+++ apps/phone-shop/src/app/app.module.ts
@@ -7,20 +7,26 @@ import {
 } from '@tuskphone/feature-phones';
 import { RouterModule } from '@angular/router';
 import { PhonesComponent, PhoneDetailComponent } from '@tuskphone/feature-phones';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FeaturePhonesModule, RouterModule.forRoot([{
-    path: 'phones',
-    component: PhonesComponent
-  },{
-    path: 'phones/:id',
-    component: PhoneDetailComponent
-  },{
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'phones'
-  }])],
+  imports: [
+    BrowserModule, 
+    FeaturePhonesModule,
+    MatToolbarModule,
+    RouterModule.forRoot([{
+      path: 'phones',
+      component: PhonesComponent
+    },{
+      path: 'phones/:id',
+      component: PhoneDetailComponent
+    },{
+      path: '',
+      pathMatch: 'full',
+      redirectTo: 'phones'
+    }])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
