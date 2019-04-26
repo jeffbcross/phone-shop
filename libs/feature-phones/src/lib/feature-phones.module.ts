567b4a94b55f8bcd35109784acc2af11c923199a
Kaitlyn <kaitlyn.ekdahl@gmail.com>
2019-04-25 21:44:54 -0700

    Initial styles and quick unit test fixes

--- libs/feature-phones/src/lib/feature-phones.module.ts
+++ libs/feature-phones/src/lib/feature-phones.module.ts
@@ -4,9 +4,15 @@ import { RouterModule, Route } from '@angular/router';
 import { PhonesComponent } from './phones/phones.component';
 import { PhoneDetailComponent } from './phone-detail/phone-detail.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HttpClientModule,
+    FlexLayoutModule
+  ],
   declarations: [PhonesComponent, PhoneDetailComponent],
   exports: [PhonesComponent, PhoneDetailComponent]
 })
